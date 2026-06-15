b15d6f50085bc1f2a54c16ba7753c2db6ffca66a
wwwmouse <2433154101@qq.com>
2026-06-15 14:38:39 +0800

    修改图2为消费时段分布

--- projects/wechat-bill/analysis.py
+++ projects/wechat-bill/analysis.py
@@ -5,7 +5,7 @@ import os
 def save_chart(filename):
     """保存图表到 results-pictures 文件夹"""
     base_dir = os.path.dirname(__file__)
-    save_dir = os.path.join(base_dir, 'results-pictures')
+    save_dir = os.path.join(BASE_DIR, 'results-pictures')
     os.makedirs(save_dir, exist_ok=True)
     
     plt.savefig(
@@ -19,7 +19,12 @@ from matplotlib import rcParams
 rcParams['font.family']= 'SimHei'
 #windows处理中文
 
-preview = pd.read_excel('projects/wechat-bill/datas.xlsx', header=None, nrows=50)#先截前50行找数据起点位置
+# 代码文件所在目录（E:\VScode\wechat-bill）
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# 拼接路径
+data_path = os.path.join(BASE_DIR, 'datas.xlsx')
+preview = pd.read_excel(data_path, header=None, nrows=50)#先截前50行找数据起点位置
 
 header_row = None
 for i in range(len(preview)):
@@ -29,12 +34,12 @@ for i in range(len(preview)):
     
 del preview #手动释放
 
-datas = pd.read_excel('projects/wechat-bill/datas.xlsx', header=header_row)#从列索引那一行开始读
+datas = pd.read_excel(data_path, header=header_row)#从列索引那一行开始读
 
-# print(datas.isna().sum())
+print(datas.isna().sum())
 # 空白检查，确认每列都没有空的
 
-# print(datas.duplicated().sum())
+print(datas.duplicated().sum())
 # 重复检查，确认没重复内容
 
 datas['交易年月']=datas['交易时间'].dt.to_period('M')
@@ -55,7 +60,7 @@ datas = datas[~datas['商品'].str.contains('/', na=False)]
 datas=datas.sort_values('交易年月')
 datasm=datas.groupby(['交易年月','收/支'],as_index=False)['金额(元)'].sum()
 #分组聚合，形成每个月总金额图
-print(datasm)
+#print(datasm)
 
 datasm_w=datasm.pivot(index='交易年月',columns='收/支',values='金额(元)')
 datasm_w=datasm_w.reset_index() #令交易年月从索引变为普通列，不然没法从列取时间
@@ -63,6 +68,7 @@ datasm_w.columns.name=None
 
 print(datasm_w)
 # 长表变宽表，分开收/支
+
 expend=datasm_w['支出'].round(2)
 income=datasm_w['收入'].round(2)
 
@@ -113,21 +119,34 @@ plt.tight_layout()
 save_chart("月度收支趋势图.png")
 plt.show()
 
-# 2.绘制交易时间条形图
+# 2.绘制消费时间条形图
 datas['交易小时']=datas['交易时间'].dt.hour
-datash=datas.groupby("交易小时")["金额(元)"].sum()
+datash=datas.groupby(["交易小时","收/支"],as_index=False)["金额(元)"].sum()
+#分组聚合成长表，分别每个小时的支出总和与收入总和
 
-# 补齐0-23小时，没有的填0
-datash = datash.reindex(range(24), fill_value=0)
+expend=datash[datash['收/支']=='支出'].round(2)
+#筛选支出总和，制成新表
 
-datash=datash.reset_index()
+expend=expend.pivot(index='交易小时',columns='收/支',values='金额(元)')
+#以交易小时为index,收/支为column(此时横轴只剩支出),金额为values制成新表
 
-print(datash)
-print(f"交易金额最大的小时:{datash.iloc[datash['金额(元)'].idxmax(),0]}")
-print(f"交易金额：{round(datash['金额(元)'].max(),2)}")
+expend=expend.reindex(range(24), fill_value=0)
+#存在有些时间段没有任何支出，需要在这里补齐0-23小时，没有的填0
 
-day_hours=datash["交易小时"]
-day_money=datash["金额(元)"].round(2)
+expend=expend.reset_index()
+#将'交易小时'从index变为普通列
+
+expend.columns.name=None
+#去掉columns.name收/支
+
+print(expend)
+
+
+print(f"消费金额最大的小时:{expend.iloc[expend['支出'].idxmax(),0]}")
+print(f"该小时消费总金额:{round(expend['支出'].max(),2)}")
+
+day_hours=expend["交易小时"]
+day_money=expend["支出"].round(2)
 
 colors = ['#2c3e50' if h < 6 else    # 凌晨深蓝
           '#f39c12' if h < 12 else   # 上午橙色
@@ -138,13 +157,12 @@ plt.bar(day_hours, day_money, color=colors, width=0.5)
 
 plt.title("消费时段分布图",fontsize=20,color="red")
 plt.xlabel("小时",fontsize=10,color="black")
-plt.ylabel("金额",fontsize=10,color="black",rotation=0)
+plt.ylabel("支出",fontsize=10,color="black",rotation=0)
 
 plt.grid(True, axis='y', alpha=0.6, color="gray")
 
-
 for x,y in enumerate(day_money):
-     plt.text(x,y+max(day_money)*0.01,str(y),ha="center",va="bottom",fontsize=10,color='black')
+     plt.text(x,y,str(y),ha="center",va="bottom",fontsize=10,color='black')
      
 plt.tight_layout()
 save_chart("消费时段分布图.png")
