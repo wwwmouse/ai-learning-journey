b652c715a12adb3ab4154ba69b19ec52732abb34
wwwmouse <2433154101@qq.com>
2026-07-29 20:26:40 +0800

    增加新的缺失列

--- projects/titanic_project/src/explore.py
+++ projects/titanic_project/src/explore.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 matplotlib.rcParams['font.sans-serif'] = ['Microsoft YaHei', 'SimHei']
-#指定用微软雅黑或黑体渲染文字
+# 指定用微软雅黑或黑体渲染文字
 matplotlib.rcParams['axes.unicode_minus'] = False
   
 df=pd.read_csv("AI-learning/projects/titanic_project/data/train.csv")
@@ -15,28 +15,20 @@ print(df.shape) #(891,12)
 
 print(df.isna().sum())
 print(df.isna().mean()*100)
-# age缺失19.865320
-# cabin确实77.104377
+# Age缺失19.865320
+# Cabin缺失77.104377
+# Embarked缺失0.224467
 
 print(df.describe())
-# age 
-# min 0.4200
-# max 80.0000
-# mean 29.6991
 
-# fare
-# min 0.0000
-# max 512.3292
-# mean 32.2042
-
-#存活统计
+# 存活统计
 
 print(f"生存率:{df['Survived'].mean()}")
 sns.countplot(x='Survived', data=df)
 plt.title('生存人数统计 (0=死亡, 1=存活)')
 plt.savefig('AI-learning/projects/titanic_project/images/survival_count.png', dpi=150, bbox_inches='tight')
 
-#性别VS存活
+# 性别 vs 存活
 
 male_target=df[df['Sex']=="male"]
 print(f"男性存活率:{male_target['Survived'].mean()}")
@@ -49,7 +41,7 @@ sns.countplot(x='Sex', hue='Survived', data=df)
 plt.title('性别与生存关系')
 plt.savefig('AI-learning/projects/titanic_project/images/sex_survival.png', dpi=150, bbox_inches='tight')
 
-#舱位等级VS存活
+# 舱位等级 vs 存活
 
 print(f"一等舱存活率:{df[df['Pclass']==1]['Survived'].mean()}")
 print(f"二等舱存活率:{df[df['Pclass']==2]['Survived'].mean()}")
@@ -59,7 +51,7 @@ sns.countplot(x='Pclass', hue='Survived', data=df)
 plt.title('舱位等级与生存关系')
 plt.savefig('AI-learning/projects/titanic_project/images/pclass_survival.png', dpi=150, bbox_inches='tight')
 
-#年龄 vs 存活
+# 年龄 vs 存活
 
 plt.figure()
 sns.histplot(
