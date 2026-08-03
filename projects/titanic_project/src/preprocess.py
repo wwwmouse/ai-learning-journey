8adb66d691cf5009406aaa859cde536517f45149
wwwmouse <2433154101@qq.com>
2026-08-03 20:22:51 +0800

    补充预处理内笔记

--- projects/titanic_project/src/preprocess.py
+++ projects/titanic_project/src/preprocess.py
@@ -23,27 +23,40 @@ def load_and_preprocess():
 
     # 3. Sex, Embarked 转数字
     df['Sex'] = LabelEncoder().fit_transform(df['Sex'])
-    df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
-    df['Embarked'] = LabelEncoder().fit_transform(df['Embarked'])
-
+#   fit — 扫描一遍数据，记住"有哪些不同的值"（["female", "male"]）
+#   transform — 按字母顺序给每个值分配编号（female → 0，male → 1）
+#   等价于： le = LabelEncoder()       创建一个编码器实例
+#           le.fit(df['Sex'])         扫描：发现 female 和 male
+#           df['Sex'] = le.transform(df['Sex'])  替换：female→0, male→1
+    df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0]) #对于极少的缺失值，使用众数填充最保险
+    df['Embarked'] = LabelEncoder().fit_transform(df['Embarked'])   #同上
+    
     # 4. 划分 X, y，切训练/测试集
     X = df.drop('Survived', axis=1)
     y = df['Survived']
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y,
-        test_size=0.3,
-        random_state=42,
-        stratify=y
-    )
+        X, y, # 特征矩阵，标签列
+        test_size=0.3, # 训练集规模
+        random_state=42, # 固定随机种子
+        stratify=y # 分层依据，通常为标签列，作用为把数据按照这个数组的不同类别分组，保证按照相同比例拆分训练集和测试集
+    ) 
 
-    # 5. 标准化
-    # 标准化之前记下列名，之后 DataFrame 会变成 numpy 数组，列名就丢了
+    # 5. 标准化:把所有特征拉到同一个尺度上
+    # 标准化后X_train会从 DataFrame 会变成 numpy 数组，列名就丢了
+    # 因此需要提前记录列名
     feature_names = X_train.columns.tolist()
 
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    X_train = scaler.fit_transform(X_train) # 从训练集计算均值和标准差，然后应用到训练集
+    # 等价于先 fit 再 transform：
+    # fit(X_train)：计算训练集的均值和标准差，把这些数值存到 scaler 实例属性中（scaler.mean_ 和 scaler.scale_ ）
+    # transform(X_train)：用刚才存下来的均值和标准差，对训练集进行转换
+    
+    X_test = scaler.transform(X_test) # 此时scaler内已经记录了刚才fit训练集的均值和标准差，直接用其来转换测试集
+    # 为什么不能对测试集也 fit_transform：
+    # 因为"考试"时不知道考试数据的真实分布，用测试集自己的均值和标准差去标准化，等于偷看了考试数据
+    # 造成数据泄露（data leakage），导致模型评估结果虚高，是初学者最容易犯的错误之一
 
     print(f'预处理完成。训练集形状: {X_train.shape}, 测试集形状: {X_test.shape}')
     return X_train, X_test, y_train, y_test, feature_names
